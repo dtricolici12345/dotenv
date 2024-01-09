b5c8273176e4bd139cddf6c8c67dc12c5dd39ad6
Daniela Tricolici <dtricolici12345@gmail.com>
2024-01-09 19:06:57 +0100

    Initial commit

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const name = process.env.MY_NAME;
 const city = process.env.MY_CITY;
 const language = process.env.MY_LANGUAGE;
 
-console.log(`I am ${name}, wilder in ${city}, and I love ${language}.`);
+console.log(`I am ${name}, wilder in ${city}, and I love ${language}.`);т
